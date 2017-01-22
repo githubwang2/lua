014ba146e02a20a41f5f74d38053cd7ba102d8ad
wang2 <pokemonwang2@126.com>
2017-01-22 23:41:08 +0800

    lua_game

---
+++ MainScene.lua
@@ -0,0 +1,36 @@
+
+local MenuScene = class("MenuScene", function()
+    return display.newScene("MenuScene")
+end)
+
+function MenuScene:ctor()
+	-- 1.加载精灵帧
+    display.addSpriteFrames("fruit.plist", "fruit.png")
+
+    -- 2.背景图片
+	display.newSprite("mainBG.png")
+		:pos(display.cx,display.cy)
+		:addTo(self)
+
+	local startBtnImages = {
+		normal = "#startBtn_N.png",
+		pressed = "#startBtn_S.png",
+	}
+    
+	-- 3.开始按钮
+    cc.ui.UIPushButton.new(startBtnImages, {scale9 = false})
+		:onButtonClicked(function(event)
+			local playScene = import("app.scenes.PlayScene"):new()
+			display.replaceScene(playScene, "turnOffTiles", 0.5)
+		end)
+		:align(display.CENTER, display.cx, display.cy - 80)
+		:addTo(self)
+end
+
+function MenuScene:onEnter()
+end
+
+function MenuScene:onExit()
+end
+
+return MenuScene
